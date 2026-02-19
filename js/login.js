3cbb51951cd894908ef0dc92dac1971fb43e8aa1
Wasif Hasan <wasifhasancse@gmail.com>
2026-02-19 19:38:08 +0600

    add dashboard

--- js/login.js
+++ js/login.js
@@ -1,19 +1,23 @@
 document.getElementById("login-btn").addEventListener("click", function () {
-  userNumber = '01234567890';
-  userPin = '1234';
+  userNumber = "01234567890";
+  userPin = "1234";
   const mobileNumber = getInputValue("mobile-number");
   const pin = getInputValue("pin");
-  
+
+  function modalOpen() {
+    const modal = document.getElementById("my_modal_1");
+    modal.querySelector(".modal-box p").textContent =
+      "Invalid mobile number or pin. Please try again.";
+    modal.showModal();
+    return modal;
+  }
+
   if (mobileNumber === userNumber && pin === userPin) {
     window.location.href = "./dashboard.html";
   } else {
-
-    const modal = document.getElementById("my_modal_1");
-    modal.querySelector(".modal-box p").textContent = "Invalid mobile number or pin. Please try again.";
-    modal.showModal();
-
+    const modal = modalOpen();
     setTimeout(() => {
       modal.close();
-    }, 2000);
+    }, 3000);
   }
 });
