a1395ef6f62d4fac2f8547231ea721c271a56b2e
Wasif Hasan <wasifhasancse@gmail.com>
2026-02-20 08:02:36 +0600

    develop Transfer Money section

---
+++ js/transfer-money.js
@@ -0,0 +1,40 @@
+document.getElementById("transfer-btn").addEventListener("click", function () { 
+  const transferAccountNumber = document.getElementById("transfer-account-number").value;
+  const transferAmount = Number(document.getElementById("transfer-amount").value);
+  const transferPin = document.getElementById("transfer-pin").value;
+  const currentBalance = getBlance();
+
+  if (transferAccountNumber === "" || transferAccountNumber.length !== 11) {
+    const modal = modalOpen("Please enter a valid 11-digit account number.");
+    setTimeout(() => {
+      modal.close();
+    }, 3000);
+    return;
+  }
+  if (transferAmount === "" || isNaN(transferAmount) || transferAmount <= 0 || transferAmount > currentBalance) {
+    const modal = modalOpen("Please enter a valid amount.");  
+    setTimeout(() => {
+      modal.close();
+    }, 3000);
+    return;
+  }
+  if (transferPin === userPin) {
+    const newBalance = currentBalance - transferAmount;
+    setBlance(newBalance);
+    const modal = modalOpen(
+      `Successfully transferred ${transferAmount} BDT to account number ${transferAccountNumber}.`,
+    );  
+    setTimeout(() => {
+      modal.close();
+      document.getElementById("transfer-account-number").value = "";
+      document.getElementById("transfer-amount").value = "";
+      document.getElementById("transfer-pin").value = "";
+    }, 3000);
+  } else {
+    const modal = modalOpen("Invalid PIN. Please enter your PIN.");
+    setTimeout(() => {
+      modal.close();
+    }, 3000);
+    return;
+  }
+});
